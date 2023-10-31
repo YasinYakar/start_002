10d9625b0c192b2b2baae0530c40c8fdace8e5ad
YasinYakar <54328975+YasinYakar@users.noreply.github.com>
2023-10-31 15:00:59 +0300

    Update monday.go

--- go-demo-2/saturday/monday.go
+++ go-demo-2/saturday/monday.go
@@ -6,10 +6,12 @@ import "time"
 func monday() {
 
 today:= time.Now().Weekday()
-switch time.Monday() {
+switch time.Monday {
 
-case today+0:
-
+case today+0:{fmt.Println("Today")}
+case today+1:{fmt.Println("Tomorrow")}
+case today+2:{fmt.Println("in two days")}
+default : {fmt.Println("too far away")}
 }
 
 
